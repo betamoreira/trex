7480a029c75c292b588578c2a223877157402575
betamoreira <79289691+betamoreira@users.noreply.github.com>
2021-09-17 11:37:25 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -34,12 +34,12 @@ function preload(){
 }
 
 function setup(){
-  createCanvas(600,200);
+  createCanvas( windowWidth,windowHeight);
   
   var mensagem = "Isso é uma mensagem";
   console.log(mensagem);
   
-  trex = createSprite(50,160,20,50);
+  trex = createSprite(50,height-70,20,50);
   trex.addAnimation("running", trex_correndo);
   trex.addAnimation("collided",trex_colidiu);
   trex.scale = 0.5;
@@ -47,11 +47,11 @@ function setup(){
   trex.setCollider("circle",0,0,40);
   //trex.debug = true;
   
-  solo = createSprite(300,180,600,20);
+  /*solo = createSprite(width/2,height-50,width,2);
   solo.addImage("ground", imagemdosolo);
   solo.x = solo.width/2;
   
-  soloinvisivel = createSprite(300,190,600,10);
+  soloinvisivel = createSprite(width/2,height+50,width,125);
   soloinvisivel.visible = false;
   
   fimdejogo = createSprite(300,100);
@@ -60,7 +60,28 @@ function setup(){
   
   reiniciarjogo = createSprite(300,140);
   reiniciarjogo.addImage(reiniciarjogoimg);
+  reiniciarjogo.scale = 0.5;*/
+  
+  
+  soloinvisivel = createSprite(width/2,height-10,width,125);  
+  soloinvisivel.visible = false;
+  
+  solo = createSprite(width/2,height-100,width,2);
+  solo.addImage("ground",imagemdosolo);
+  solo.x = width/2
+  //solo.velocityX = -(6 + 3*pontuacao/100);
+  
+  fimdejogo = createSprite(width/2,height/2- 50);
+  fimdejogo.addImage(fimdejogoimg);
+  
+  reiniciarjogo = createSprite(width/2,height/2);
+  reiniciarjogo.addImage(reiniciarjogoimg);
+  
+  fimdejogo.scale = 0.5;
   reiniciarjogo.scale = 0.5;
+
+  fimdejogo.visible = false;
+  reiniciarjogo.visible = false;
   
   grupodeobstaculos = new Group();
   grupodenuvens = new Group();
@@ -77,7 +98,7 @@ function draw(){
   
   //console.log("Isto é: "+estadoJogo);
   
-  text("Score: "+pontuacao, 500,50);
+  text("Score: "+pontuacao, 30,50);
   
   if(estadoJogo === JOGAR){
     fimdejogo.visible = false;
@@ -95,9 +116,10 @@ function draw(){
      solo.x = solo.width/2;
   }
   
-    if(keyDown("space") && trex.y>=140){
+    if((touches.length > 0 || keyDown("SPACE")) && trex.y  >= height-120){
       trex.velocityY = -12;
       somSalto.play();
+      touches=[];
     }
 
     trex.velocityY = trex.velocityY + 0.8;
@@ -130,6 +152,10 @@ function draw(){
     if(mousePressedOver(reiniciarjogo)){
     reset();
   }
+    if(touches.length>0 || keyDown("SPACE")) {      
+      reset();
+      touches = []
+    }
 }
   
   trex.collide(soloinvisivel);
@@ -153,7 +179,7 @@ function reset(){
 
 function gerarNuvens(){
   if(frameCount%60===0){
-    nuvem = createSprite(600,100,40,10);
+    nuvem = createSprite(width+20,height-300,40,10);
     nuvem.addImage(imagemdanuvem);
     nuvem.scale = 0.6;
     nuvem.y = Math.round(random(10,60));
@@ -172,7 +198,7 @@ function gerarNuvens(){
 
 function gerarObstaculos(){
   if(frameCount%60===0){
-    obstaculo = createSprite(600,165,10,40);
+    obstaculo = createSprite(width,height-95,20,30);
     obstaculo.velocityX = - (6 + pontuacao/100);
     
     var num = Math.round(random(1,6));
